42b30e0ccdec665a90711425a4e20e0e83ac42b2
not-developer-exe <official.additya.singh@gmail.com>
2025-04-02 20:30:38 +0530

    Nothing much, just felt like committing 😂

--- Day1/T3/index.js
+++ Day1/T3/index.js
@@ -1,11 +1,21 @@
 let form = document.querySelector('form')
-let inp = document.querySelectorAll('#inp')
+let inp = document.querySelectorAll("input[type='text']")
+let head = document.createElement('h3')
+let body = document.querySelector('body')
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
 
-    if(inp.value = ''){
-        console.log('Empty Inputs are not allowed')
-        alert('Empty Inputs are not allowed')
-    }
+    inp.forEach(inps=>{
+        if(inps.value == ''){
+            head.innerHTML = 'Error';
+            head.style.color = 'blue'
+            body.appendChild(head)
+        }
+        else{
+            console.log(inps.value)
+            head.innerHTML = ''
+            body.appendChild(head)
+        }
+    })
 })
